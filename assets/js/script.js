d8ce098690f8e10113ea4169f267163249fcf676
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-11-28 22:03:26 -0300

    Inclusão do modo escuro

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,21 @@ function closeMenu() {
     menuBtn.style.display = "block";
 }
 
+const btn = document.getElementById("toggleTheme");
+const icon = document.getElementById("iconTheme");
+
+btn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    } else {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    }
+});
+
+
+
